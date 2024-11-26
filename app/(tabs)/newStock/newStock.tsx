2a311cd490b0c875f02e3ee8401174c014290769
Rufus B <127842229+rufusbiggs@users.noreply.github.com>
2024-11-26 17:30:54 +0000

    Actual prescription data added to Add Pills screen. Next need to get Add Stock function working

--- app/(tabs)/newStock/newStock.tsx
+++ app/(tabs)/newStock/newStock.tsx
@@ -1,68 +1,19 @@
 import { ScrollView, View, Text, StyleSheet } from "react-native"
 import AddStockCard from "../../components/AddStockCard"
 import NewPrescription from "../../components/NewPrescription"
-import { createStackNavigator } from '@react-navigation/stack';
+import { useUser } from "../../user/UserContext";
 
-const Stack = createStackNavigator();
-
-interface Prescription {
-  id: number,
-  drug: string,
-  pillDose: number,
-  dailyDose: number,
-  startDate: string,
-  initialStock: number,
-  addedPills: number[],
-}
-
-const prescriptionsData : Prescription[] = [
-  {
-  "id": 1,
-  "drug": "Lacosamide",
-  "pillDose": 50,
-  "dailyDose": 300,
-  "startDate": "2024-02-01T00:00:00.000Z",
-  "initialStock": 1692,
-  "addedPills": [50]
-  },
-  {
-  "id": 2,
-  "drug": "Levothyroxine",
-  "pillDose": 250,
-  "dailyDose": 250,
-  "startDate": "2024-02-01T00:00:00.000Z",
-  "initialStock": 392,
-  "addedPills": [50, 100]
-  },
-  {
-  "id": 3,
-  "drug": "Dexamethasone",
-  "pillDose": 50,
-  "dailyDose": 100,
-  "startDate": "2024-02-01T00:00:00.000Z",
-  "initialStock": 392,
-  "addedPills": [50, 90, 85]
-  },
-  {
-  "id": 3,
-  "drug": "Dexamethasone",
-  "pillDose": 50,
-  "dailyDose": 100,
-  "startDate": "2024-02-01T00:00:00.000Z",
-  "initialStock": 392,
-  "addedPills": [50, 90, 85]
-  }
-]
-;
 
 const NewStock = () => {
+  const { prescriptionData } = useUser();
+
   return (
     <ScrollView style={styles.main}>
       <View style={styles.descriptionContainer}>
         <Text style={styles.description}>Increase the stock of an already running prescription by selecting <Text style={{ fontWeight: 'bold', color: '#335db0'}}>Add Stock</Text>.</Text>
         <Text style={styles.description}>Add a new prescription with <Text style={{ fontWeight: 'bold', color: '#a05f3a'}}>New Prescription</Text>.</Text>
       </View>
-        {prescriptionsData.map((prescription, idx) => {
+        {prescriptionData.map((prescription, idx) => {
           return (
             <AddStockCard key={idx} prescription={prescription} />
           )
